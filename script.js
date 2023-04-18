9911a68c32bb94f830fe2909ebe26c75b196b21a
thebenjieangeles <zerofourtwentifive@gmail.com>
2023-04-18 21:08:05 +0700

    added functionality and updated css

--- script.js
+++ script.js
@@ -1,29 +1,96 @@
-// const calculator = (() => {
-//   const add = (a, b) => a + b;
-//   const sub = (a, b) => a - b;
-//   const mul = (a, b) => a * b;
-//   const div = (a, b) => a / b;
-//   return { add, sub, mul, div };
-// })();
-
-// console.log(calculator.add(3.5, 5));
-// console.log(calculator.sub(3.5, 5));
-
 const numberButtons = document.querySelectorAll(".is-num");
+const operatorButtons = document.querySelectorAll(".is-operator");
 const clearButton = document.querySelector(".is-clear");
 const largeDisplay = document.querySelector(".large-display");
+const smallDisplay = document.querySelector(".small-display");
+const btnEquals = document.querySelector(".is-equals");
 
 let currentInput = "";
 let previousInput = "";
+let operator = undefined;
+
+function clear() {
+  currentInput = "";
+  previousInput = "";
+  operator = undefined;
+}
+
+function updateDisplay() {
+  if (currentInput === "" && largeDisplay.value === "") {
+    largeDisplay.value = "";
+  } else {
+    largeDisplay.value = currentInput;
+  }
+
+  if (operator !== undefined) {
+    smallDisplay.value = previousInput + operator;
+  } else {
+    smallDisplay.value = "";
+  }
+}
+
+function chooseOperator(chosenOperator) {
+  if (currentInput === "") return;
+  if (previousInput !== "") compute();
+
+  operator = chosenOperator;
+  previousInput = currentInput;
+  currentInput = "";
+}
+
+function appendNumber(number) {
+  currentInput += number;
+}
+
+function compute() {
+  let total = 0;
+  const previousNumber = parseInt(previousInput);
+  const currentNumber = parseInt(currentInput);
+
+  switch (operator) {
+    case "+":
+      total = previousNumber + currentNumber;
+      break;
+    case "−":
+      total = previousNumber - currentNumber;
+      break;
+    case "×":
+      total = previousNumber * currentNumber;
+      break;
+    case "÷":
+      total = previousNumber / currentNumber;
+      break;
+    default:
+      return "";
+  }
+
+  currentInput = total;
+  previousInput = "";
+  operator = undefined;
+}
 
 numberButtons.forEach((button) => {
   button.addEventListener("click", (event) => {
-    currentInput += event.target.textContent;
-    largeDisplay.textContent = currentInput;
+    if (largeDisplay.value === "" && event.target.textContent === "0") return;
+
+    appendNumber(event.target.textContent);
+    updateDisplay();
   });
 });
 
 clearButton.addEventListener("click", () => {
-  currentInput = "0";
-  largeDisplay.textContent = currentInput;
+  clear();
+  updateDisplay();
+});
+
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    chooseOperator(event.target.textContent);
+    updateDisplay();
+  });
+});
+
+btnEquals.addEventListener("click", () => {
+  compute();
+  updateDisplay();
 });
